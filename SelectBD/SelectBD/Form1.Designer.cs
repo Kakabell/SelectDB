f056c00bd8d6bcbc71ca6d9c4ad8ca82cb969d20
Gustavo Aparecido de Souza Lopes <gltechlopes@gmail.com>
2024-01-12 18:03:20 -0300

    Add files via upload

---
+++ SelectBD/SelectBD/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace SelectBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtBancoDados = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnTestarConexao = new System.Windows.Forms.Button();
+            this.btnExeComando = new System.Windows.Forms.Button();
+            this.ViewSelect = new System.Windows.Forms.DataGridView();
+            this.btnGeraTxt = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtComando = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewSelect)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(812, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(26, 22);
+            this.toolStripLabel1.Text = "Sair";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Banco de Dados:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(325, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Usuario:";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtServidor.Location = new System.Drawing.Point(15, 54);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(148, 20);
+            this.txtServidor.TabIndex = 6;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsuario.Location = new System.Drawing.Point(329, 54);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(148, 20);
+            this.txtUsuario.TabIndex = 7;
+            // 
+            // txtBancoDados
+            // 
+            this.txtBancoDados.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBancoDados.Location = new System.Drawing.Point(15, 105);
+            this.txtBancoDados.Name = "txtBancoDados";
+            this.txtBancoDados.Size = new System.Drawing.Size(148, 20);
+            this.txtBancoDados.TabIndex = 8;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSenha.Location = new System.Drawing.Point(328, 105);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(148, 20);
+            this.txtSenha.TabIndex = 9;
+            // 
+            // btnTestarConexao
+            // 
+            this.btnTestarConexao.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTestarConexao.Location = new System.Drawing.Point(567, 38);
+            this.btnTestarConexao.Name = "btnTestarConexao";
+            this.btnTestarConexao.Size = new System.Drawing.Size(233, 45);
+            this.btnTestarConexao.TabIndex = 10;
+            this.btnTestarConexao.Text = "Conectar";
+            this.btnTestarConexao.UseVisualStyleBackColor = true;
+            this.btnTestarConexao.Click += new System.EventHandler(this.btnTestarConexao_Click);
+            // 
+            // btnExeComando
+            // 
+            this.btnExeComando.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExeComando.Location = new System.Drawing.Point(567, 193);
+            this.btnExeComando.Name = "btnExeComando";
+            this.btnExeComando.Size = new System.Drawing.Size(233, 45);
+            this.btnExeComando.TabIndex = 11;
+            this.btnExeComando.Text = "Executar Comando";
+            this.btnExeComando.UseVisualStyleBackColor = true;
+            this.btnExeComando.Click += new System.EventHandler(this.btnExeComando_Click);
+            // 
+            // ViewSelect
+            // 
+            this.ViewSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ViewSelect.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ViewSelect.Location = new System.Drawing.Point(0, 310);
+            this.ViewSelect.Name = "ViewSelect";
+            this.ViewSelect.Size = new System.Drawing.Size(812, 332);
+            this.ViewSelect.TabIndex = 12;
+            // 
+            // btnGeraTxt
+            // 
+            this.btnGeraTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGeraTxt.Location = new System.Drawing.Point(12, 648);
+            this.btnGeraTxt.Name = "btnGeraTxt";
+            this.btnGeraTxt.Size = new System.Drawing.Size(118, 41);
+            this.btnGeraTxt.TabIndex = 13;
+            this.btnGeraTxt.Text = "Gerar TXT";
+            this.btnGeraTxt.UseVisualStyleBackColor = true;
+            this.btnGeraTxt.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(682, 648);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 41);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Sair";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Comando:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtComando
+            // 
+            this.txtComando.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtComando.Location = new System.Drawing.Point(67, 143);
+            this.txtComando.Multiline = true;
+            this.txtComando.Name = "txtComando";
+            this.txtComando.Size = new System.Drawing.Size(494, 161);
+            this.txtComando.TabIndex = 16;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 707);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(812, 22);
+            this.statusStrip1.TabIndex = 17;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 729);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtComando);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnGeraTxt);
+            this.Controls.Add(this.ViewSelect);
+            this.Controls.Add(this.btnExeComando);
+            this.Controls.Add(this.btnTestarConexao);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtBancoDados);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select Comand";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewSelect)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtBancoDados;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnTestarConexao;
+        private System.Windows.Forms.Button btnExeComando;
+        private System.Windows.Forms.DataGridView ViewSelect;
+        private System.Windows.Forms.Button btnGeraTxt;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtComando;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
---
+++ SelectBD/SelectBD/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SelectBD
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString;
+        private string ultimoComandoExecutado;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTestarConexao_Click(object sender, EventArgs e)
+        {
+            string servidor = txtServidor.Text;
+            string bancoDados = txtBancoDados.Text;
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
+
+            string connectionString = $"Data Source={servidor};Initial Catalog={bancoDados};User ID={usuario};Password={senha}";
+
+            using (SqlConnection conexao = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    // Conexão bem-sucedida, armazene os dados e exiba o nome do banco de dados
+                    MessageBox.Show($"Conexão bem-sucedida com o banco de dados '{bancoDados}'", "Teste de Conexão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    statusLabel.Text = $"Conectado ao banco de dados: {bancoDados}";
+                }
+                catch (Exception ex)
+                {
+                    // Erro ao conectar, exiba uma mensagem de erro
+                    MessageBox.Show($"Erro ao conectar: {ex.Message}", "Erro de Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    statusLabel.Text = "Erro ao conectar";
+                }
+
+                finally
+                {
+                    // Garanta que a conexão seja sempre fechada, mesmo em caso de erro
+                    if (conexao.State == System.Data.ConnectionState.Open)
+                        conexao.Close();
+                }
+
+
+            }
+        }
+
+        private async void btnExeComando_Click(object sender, EventArgs e)
+        {
+
+            
+
+            string servidor = txtServidor.Text;
+            string bancoDados = txtBancoDados.Text;
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
+
+            string connectionString = $"Data Source={servidor};Initial Catalog={bancoDados};User ID={usuario};Password={senha}";
+
+            string comandoSql = txtComando.Text.Trim();
+
+            using (SqlConnection conexao = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    await conexao.OpenAsync();
+
+                    using (SqlCommand comando = new SqlCommand(comandoSql, conexao))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(comando);
+                        DataTable dataTable = new DataTable();
+
+                        // Use o método FillAsync para permitir que a barra de progresso seja atualizada durante o preenchimento dos dados
+                        await Task.Run(() => adapter.Fill(dataTable));
+
+                        // Exibe os dados no DataGridView
+                        ViewSelect.DataSource = dataTable;
+                    }
+
+                    ultimoComandoExecutado = comandoSql;
+                    statusLabel.Text = "Comando executado com sucesso.";
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Erro ao executar o comando: {ex.Message}", "Erro de Comando", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    statusLabel.Text = "Erro ao executar o comando.";
+                }
+                finally
+                {
+                    if (conexao.State == ConnectionState.Open)
+                        conexao.Close();
+
+                    
+                }
+            }
+        }
+
+        
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ultimoComandoExecutado))
+            {
+                try
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Arquivo de Texto|*.txt";
+                    saveFileDialog.Title = "Salvar Resultado em TXT";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string caminhoArquivo = saveFileDialog.FileName;
+
+                        using (StreamWriter writer = new StreamWriter(caminhoArquivo))
+                        {
+                            // Escreve o comando executado no arquivo
+                            writer.WriteLine("############################################");
+                            writer.WriteLine($"Comando Executado: {ultimoComandoExecutado}");
+                            writer.WriteLine("############################################");
+                            writer.WriteLine();
+
+                            // Escreve os cabeçalhos das colunas
+                            foreach (DataGridViewColumn coluna in ViewSelect.Columns)
+                            {
+                                writer.Write($"{coluna.HeaderText}\t");
+                            }
+                            writer.WriteLine();
+
+                            // Escreve os dados do DataGridView em formato tabular
+                            foreach (DataGridViewRow linha in ViewSelect.Rows)
+                            {
+                                foreach (DataGridViewCell cell in linha.Cells)
+                                {
+                                    writer.Write($"{cell.Value}\t");
+                                }
+                                writer.WriteLine();
+                            }
+
+                            MessageBox.Show("Arquivo TXT gerado com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao gerar o arquivo TXT: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não há dados para serem salvos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    
+    }
+}
